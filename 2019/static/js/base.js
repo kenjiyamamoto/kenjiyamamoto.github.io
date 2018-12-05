56c7fe5bcd53096f6680bc689e741574639771e8
Kenji Yamamoto <kenji@corp.globo.com>
2018-12-05 16:39:44 -0200

    ajuste no instagram

--- 2019/static/js/base.js
+++ 2019/static/js/base.js
@@ -1,7 +1,10 @@
 // Init Instagram
 var feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'awesome',
-    clientId: '874257cd75be4d578c56c1ade1330740'
+    get: 'user',
+    userId: '7608',
+    sortBy: 'most-liked',
+    limit: 10,
+    clientId: '874257cd75be4d578c56c1ade1330740',
+    accessToken: '7608.874257c.e5761d42f4434531ae0057113d6c41fe'
 });
-feed.run();
+// feed.run();
